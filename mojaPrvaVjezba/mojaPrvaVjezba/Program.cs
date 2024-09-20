f4a79a6bd8ddbca58c0488ef4b8eccc395d7995a
LeonCicak <leoncicak07@gmail.com>
2024-09-20 17:00:35 +0200

    Add files via upload

---
+++ mojaPrvaVjezba/mojaPrvaVjezba/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mojaPrvaVjezba
+{
+    internal class Program
+    {
+        static void Main(string[] args);
+            int a, b;
+            double c, d, x;
+            a = 7;
+            b = 10;
+            c = 12.5;
+            d = 16.7;
+            x = a + b + c + d / 2;
+            string ispis = " prosjecna vrijednost je " + x;
+        Console.WriteLine(ispis);
+            Conosole.ReadKey();
+
+        {
+        }
+    }
+}
